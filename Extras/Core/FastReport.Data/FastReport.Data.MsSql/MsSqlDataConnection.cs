21c7b4cd096ce7e12eb991fc203d1074abdb534f
Mike Eckel <mike.eckel@education.ne.gov>
2026-07-16 11:23:35 -0500

    package updates

--- Extras/Core/FastReport.Data/FastReport.Data.MsSql/MsSqlDataConnection.cs
+++ Extras/Core/FastReport.Data/FastReport.Data.MsSql/MsSqlDataConnection.cs
@@ -1,9 +1,10 @@
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Text;
 
 namespace FastReport.Data
 {
